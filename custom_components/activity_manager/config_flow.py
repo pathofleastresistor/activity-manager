151ba6c439729281ba926b12a061c575ebea3e39
pathofleastresistor <byamitvaria@gmail.com>
2025-03-09 15:08:04 -0700

    Improve handling for single entry

--- custom_components/activity_manager/config_flow.py
+++ custom_components/activity_manager/config_flow.py
@@ -1,6 +1,8 @@
 """Config flow for ActivityManager."""
 import voluptuous as vol
 from homeassistant import config_entries
+from homeassistant.data_entry_flow import FlowResult
+from homeassistant.core import callback
 
 from .const import DOMAIN  # import the domain from const.py
 import logging
@@ -15,10 +17,32 @@ class ActivityManagerFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
+
         # Check if already configured
+        if self._async_current_entries():
+            return self.async_abort(reason="single_instance_allowed")
+        
         await self.async_set_unique_id(DOMAIN)
         self._abort_if_unique_id_configured()
 
-        return self.async_create_entry(title='Activity Manager', data={'name': 'Activity Manager'})
+        return self.async_create_entry(title="Activity Manager", data={'name': "Activity Manager"})
 
     async_step_import = async_step_user
+    
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return ActivityManagerOptionsFlowHandler(config_entry)
+
+
+class ActivityManagerOptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle options for the Activity Manager component."""
+
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        """Initialize ActivityManager options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+        return self.async_abort(reason="single_instance_allowed")
